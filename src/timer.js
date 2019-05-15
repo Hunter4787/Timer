c198226ccc5ae61873bfc4768105a4e67bbc244c
Khaled AMARI <khaled_amari@live.fr>
2019-05-15 10:17:46 +0200

    first commit

---
+++ src/timer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Timer = () =>{
+    return (<div className="timer-style">
+        <div className="cpnt-style">
+        <span>00:</span>
+        <p>Hour</p>
+        </div>
+
+
+        <div className="cpnt-style">
+        <span>30:</span>
+        <p>Minute</p>
+        </div>
+
+
+        <div className="cpnt-style">
+        <span>00</span>
+        <p>Second</p>
+        </div>
+
+        
+        
+        </div>
+    )
+}
+export default Timer
